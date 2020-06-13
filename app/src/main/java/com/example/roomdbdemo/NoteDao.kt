3fba444407d5c7b704353457c40b4a74d6372d86
ankitbajaj1989 <ankitbajaj1989@gmail.com>
2020-06-13 13:38:47 +0530

    Added Dao class

---
+++ app/src/main/java/com/example/roomdbdemo/NoteDao.kt
@@ -0,0 +1,29 @@
+package com.example.roomdbdemo
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+/**
+ *Created by Ankit Bajaj on 13-06-2020.
+ */
+@Dao
+interface NoteDao {
+
+    @Insert
+    fun insert(note: Note)
+
+    @Update
+    fun update(note: Note)
+
+    @Delete
+    fun delete(note: Note)
+
+    // Room dont have all the notation available for all the different types of operation so there is way to cover this
+
+    // So writing query we are getting ide suggestions as well as doing typo mistake will be compilination error
+    @Query("delete from note_table")
+    fun deleteAllNotes()
+
+    @Query("select * from note_table ORDER BY prority DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+}
